2c5a73d90b2e35b7f72959f35f0a27a0f1a93cb7
kchen53 <kellychen@ufl.edu>
2024-11-24 20:23:09 -0500

    changed complementary_color function to return hexstring of color and added notes to function

--- BACKEND/colorAnalysis/backendApp/algorithms/complementary.py
+++ BACKEND/colorAnalysis/backendApp/algorithms/complementary.py
@@ -1,18 +1,25 @@
 from conversion import *
 
-def complementary_color(hue):
+#determines the complementary color based on hue and return the color's hex string
+def complementary_color(hue, saturation, value):
+
+    #find opposite color
     hue = hue + 180
 
+    #to keep hue within color wheel range
     if hue > 360:
         hue = hue - 360
     
-    return hue
+    #conversion algo to get hex string of complementary color
+    r, g, b = HSV_To_RBG(hue, saturation, value)
+    compColor = RGB_To_Hex(r, g, b)
+    
+    return compColor
 
 #Testing
 r, g, b = Hex_To_RGB("#FFFF00")
 hue, saturation, value = RGB_To_HSV(r, g, b)
 print(r, g, b)
 print(hue, saturation, value)
-print(complementary_color(hue))
-print(HSV_To_RBG(complementary_color(hue), saturation, value))
+print(complementary_color(hue, saturation, value))
 print(RGB_To_Hex(r, g, b))
